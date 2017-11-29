8fd6cb7081076e91703eaa13703e08fd0b3d0964
Ranjith Kumar <brk1686@gmail.com>
2017-11-28 21:49:24 -0500

    Added welcome message

--- ObjectiveC/ObjectiveC/main.m
+++ ObjectiveC/ObjectiveC/main.m
@@ -12,6 +12,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"welcome to xcode");
+        NSString *s = @"Welcome to xcode";
+        NSString * s1 = @"user123";
+        NSString *welcomeMessage = [s stringByAppendingString:s1];
+        NSLog(@"%@",welcomeMessage);
     }
     return 0;
 }
