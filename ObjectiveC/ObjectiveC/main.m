fcd06fad52728824e93d78864646491532065620
Ranjith Kumar <brk1686@gmail.com>
2017-11-28 21:25:44 -0500

    created initial project

---
+++ ObjectiveC/ObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveC
+//
+//  Created by Ranjith Kumar on 11/28/17.
+//  Copyright © 2017 XYZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
